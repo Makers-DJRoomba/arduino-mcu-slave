a26a6bd70c6aa3e5bc6c18f6272e4e04f6a0c1f9
Yutong Gu <gyutong@gmil.com>
2019-10-24 23:26:54 -0700

    added support for feather_m0

--- freeRTOS/heap_4bis.c.d
+++
@@ -1,5 +0,0 @@
-freeRTOS/heap_4bis.c.o: freeRTOS/heap_4bis.c freeRTOS/FreeRTOS.h \
- freeRTOS/FreeRTOSConfig.h freeRTOS/projdefs.h freeRTOS/portable.h \
- freeRTOS/deprecated_definitions.h freeRTOS/portmacro.h \
- freeRTOS/mpu_wrappers.h freeRTOS/error_hooks.h freeRTOS/task.h \
- freeRTOS/list.h freeRTOS/queue.h freeRTOS/semphr.h
--- freeRTOS/idle_hook.c.d
+++
@@ -1,5 +0,0 @@
-freeRTOS/idle_hook.c.o: freeRTOS/idle_hook.c freeRTOS/FreeRTOS.h \
- freeRTOS/FreeRTOSConfig.h freeRTOS/projdefs.h freeRTOS/portable.h \
- freeRTOS/deprecated_definitions.h freeRTOS/portmacro.h \
- freeRTOS/mpu_wrappers.h freeRTOS/error_hooks.h freeRTOS/FreeRTOS.h \
- freeRTOS/task.h freeRTOS/list.h freeRTOS/queue.h freeRTOS/semphr.h
