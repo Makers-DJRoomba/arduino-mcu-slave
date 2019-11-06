3c6732d1292a125c1d8566655763ba6ea515c312
Andrew Opem <andrewopem@gmail.com>
2019-11-06 13:31:36 -0800

    get encoder speed using interrupt

--- custom-lib/robot-drivers/encoder.h
+++ custom-lib/robot-drivers/encoder.h
@@ -6,13 +6,13 @@ class Encoder {
   public:
     Encoder(const int pin);
     ~Encoder(); 
-    int getValue();
+    float getSpeed();
 
   private:
-    static void increment();
-    static int _ticks = 0;
-    static TaskHandle_t increment_Task;
-    SemaphoreHandle_t tick_Semaphore = NULL;
-};
+    static void timePerTick();
+    static unsigned long _time;
+    static unsigned long _old_time;
+    const int _pin;
+  };
 
 #endif
