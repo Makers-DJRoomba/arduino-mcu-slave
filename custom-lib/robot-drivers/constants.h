5e91381d020f55bdd69347a269e30cc1aaf5bc27
Andrew Opem <andrewopem@gmail.com>
2019-11-06 13:32:12 -0800

    header file for constants - may be unnecessary in future

---
+++ custom-lib/robot-drivers/constants.h
@@ -0,0 +1,13 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define _USE_MATH_DEFINES
+
+#include <cmath>
+
+const int TicksPerRev        = 0; // define
+const int WheelDiameter      = 0; // define
+const int WheelDistance      = 0; // define
+const float DistancePerClick = (M_PI * WheelDiameter)/TicksPerRev;
+
+#endif
