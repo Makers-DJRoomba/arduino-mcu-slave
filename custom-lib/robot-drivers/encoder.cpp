2d334c281672c01026b70ddbf0603f5eb281f53b
Andrew Opem <andrewopem@gmail.com>
2019-11-06 13:31:36 -0800

    get encoder speed using interrupt

--- custom-lib/robot-drivers/encoder.cpp
+++ custom-lib/robot-drivers/encoder.cpp
@@ -1,74 +1,31 @@
 #include "Arduino.h"
 #include "encoder.h"
-
-#include <FreeRTOS_SAMD21.h>
+#include "constants.h"
 
 // https://www.electroschematics.com/arduino-optical-position-rotary-encoder/
 // http://www.ekt2.com/pdf/412_CH_SMART_SHAFT_ENCODER_KIT.pdf
-Encoder::Encoder(const int pin) {
-  pinMode(pin, INPUT);
+Encoder::Encoder(const int pin)
+  : _pin(pin) {
+  pinMode(_pin, INPUT);
+  attachInterrupt(_pin, timePerTick, RISING);
 
-  static int arg = pin;
-
-  xTaskCreate(increment,          // task function
-              "Poll Encoder",     // ID string
-              128,                // stack size
-              &arg,               // parameters
-              DEFAULT_PRIORITY,   // priority 
-              &increment_Task);   // function handle
-
-  // create semaphore for accessing tick value
-  tick_Semaphore = xSemaphoreCreateMutex();
-
-  // could do it this way with an interrupt instead
-  // attachInterrupt(pin, [&_ticks](){ _ticks++; }, RISING);
+  // maybe can do this way and get rid of extra function
+  // attachInterrupt(_pin, [this]() { _old_time = _time; _time = millis(); }, RISING);
 }
 
 Encoder::~Encoder() {
-  if (increment_Task != NULL)
-    vTaskDelete(increment_Task);
-  vSemaphoreDelete(tick_Semaphore);
   _ticks = 0;
+  detachInterrupt(_pin);
 }
 
-int Encoder::getValue() {
-  if (tick_Semaphore != NULL) {
-
-    // will block until _ticks can be returned, 
-    // otherwise there is an error returned using -1
-    if (xSemaphoreTake(tick_Semaphore, portMAX_DELAY) == pdTRUE) {
-      return _ticks;
-      xSemaphoreGive(tick_Semaphore);
-    } else {
-      return -1;
-    }
-  }
+float Encoder::getSpeed() {
+  // formula = (NumTicks*DistancePerClick)/elapsed_time
+  // assume 1 tick passed between each interrupt
+  // units: (tick distance)/s
+  return DistancePerClick/(_time / 1000.0f - _old_time / 1000.0f);
 }
 
-static void Encoder::increment(void* pin) {
-  int pin = *(int*)pin; 
-  int pin_state = 0;
-  int old_pin_state = 0;
-
-  for (;;) {
-    pin_state = digitalRead(pin);
-
-    // increment on low to high transition
-    if ((old_pin_state == LOW) && (pin_state == HIGH)) {
-      if (tick_Semaphore != NULL) {
-
-        // if semaphore not locked, increment _ticks and give back lock
-        if (xSemaphoreTake(tick_Semaphore, portMAX_DELAY) == pdTRUE) {
-          _ticks++;
-          xSemaphoreGive(tick_Semaphore);
-        }
-      }
-    }
-
-    old_pin_state = pin_state;
-  }
+static void timePerTick() {
+  _old_time = _time;  
+  _time     = millis();
 }
-
-
-
-
