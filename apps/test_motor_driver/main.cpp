885acaf3244d2db1dcc469f1964a9a22ed3acc92
Yutong Gu <gyutong@gmil.com>
2019-11-08 21:51:45 -0800

    added test application for motor_driver

---
+++ apps/test_motor_driver/main.cpp
@@ -0,0 +1,73 @@
+/*
+  Copyright (c) 2015 Arduino LLC.  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+  See the GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#define ARDUINO_MAIN
+#include "Arduino.h"
+#include <robot-drivers/motor_driver.cpp>
+
+// Weak empty variant initialization function.
+// May be redefined by variant files.
+void initVariant() __attribute__((weak));
+void initVariant() { }
+
+// Initialize C library
+extern "C" void __libc_init_array(void);
+
+/*
+ * \brief Main entry point of Arduino application
+ */
+int main( void )
+{
+  init();
+
+  __libc_init_array();
+
+  initVariant();
+
+  delay(1);
+
+#if defined(USE_TINYUSB)
+  Adafruit_TinyUSB_Core_init();
+#elif defined(USBCON)
+  USBDevice.init();
+  USBDevice.attach();
+#endif
+
+  //setup();
+  MotorDriver md = MotorDriver(9,10,11,12,0)
+
+  for (;;)
+  {
+    //loop();
+    md.driveMotorA(50);
+    md.driveMotorB(50);
+
+    delay(1000);
+
+    md.driveMotorA(25);
+    md.driveMotorB(25);
+
+    delay(1000);
+
+    yield(); // yield run usb background task
+
+    if (serialEventRun) serialEventRun();
+  }
+
+  return 0;
+}
