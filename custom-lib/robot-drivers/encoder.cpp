372c57d389be86e045f9a1d12083525fcf5e245b
Yutong Gu <gyutong@gmil.com>
2020-02-05 22:07:59 -0800

    increased tick resolution and got the encoders to work.. facing issues with floating point operation

--- custom-lib/robot-drivers/encoder.cpp
+++ custom-lib/robot-drivers/encoder.cpp
@@ -1,24 +1,36 @@
 #include "Arduino.h"
+#undef max
+#undef min
 #include "encoder.h"
 #include "constants.h"
+
+void cbUpdateTickDiff(uint32_t pinHandle) {
+  Encoder* targetEncoder = pinHandleRegister[pinHandle];
+  if(targetEncoder != NULL){
+    targetEncoder->_updateTickDiff();
+  }
+}
+
+void cbEncoderTimeOut(TimerHandle_t timerHandle){
+  Encoder* targetEncoder = timerHandleRegister[timerHandle];
+  if(targetEncoder != NULL){
+    targetEncoder->_encoderTimeOut();
+  }
+}
+
 
 // https://www.electroschematics.com/arduino-optical-position-rotary-encoder/
 // http://www.ekt2.com/pdf/412_CH_SMART_SHAFT_ENCODER_KIT.pdf
 
-typedef void (*DiffFuncPtr)();
-typedef void (*TimeFuncPtr)(void *);
 Encoder::Encoder(const int pin)
   : _pin(pin), _last_tick(xTaskGetTickCount()) {
 
-  auto cbTimeOut  = [this](){ cbEncoderTimeOut(this); };
-  auto cbTickDiff = [this](){ cbUpdateTickDiff(this); };
-
-  _encoderTimer = xTimerCreate ( /* Just a text name, not used by the RTOS
+  _timerHandle = xTimerCreate ( /* Just a text name, not used by the RTOS
                      kernel. */
                      "Timer",
                      /* The timer period in ticks, must be
                      greater than 0. */
-                     0.2 * configTICK_RATE_HZ,
+                     0.3 * configTICK_RATE_HZ,
                      /* The timer will auto-reload themselves
                      when they expire. */
                      pdTRUE,
@@ -28,22 +40,40 @@ Encoder::Encoder(const int pin)
                      ( void * ) 0,
                      /* Each timer calls the same callback when
                      it expires. */
-                     (TimeFuncPtr)&cbTimeOut
+                     (TimerCallbackFunction_t) &cbEncoderTimeOut
                    );
 
   pinMode(_pin, INPUT);
 
-  attachInterrupt(_pin, (DiffFuncPtr)&cbTickDiff, RISING);
-  xTimerStart(_encoderTimer, 10);  
+  _pinIntHandle = attachInterrupt(_pin, (voidFuncPtr) &cbUpdateTickDiff, RISING);
+  
+  registerTimerHandle(_timerHandle);
+  registerPinIntHandle(_pinIntHandle);
+
+  xTimerStart(_timerHandle, 0);  
 }
 
 Encoder::~Encoder() {
   detachInterrupt(_pin);
-  xTimerStop(_encoderTimer, 10);
+  xTimerStop(_timerHandle, 0);
+  unregisterTimerHandle(_timerHandle);
+  unregisterPinIntHandle(_pinIntHandle);
+}
+
+TimerHandle_t Encoder::getTimerHandle(){
+  return _timerHandle;
+}
+
+uint32_t Encoder::getPinIntHandle(){
+  return _pinIntHandle;
+}
+
+TickType_t Encoder::getTickDiff() {
+  return _tick_diff;
 }
 
 float Encoder::getSpeed() {
-  float time_elapsed = _tick_diff / configTICK_RATE_HZ; // seconds per slit
+  float time_elapsed = ((float)_tick_diff) / configTICK_RATE_HZ; // seconds per slit
 
   // formula = (NumTicks*DistancePerClick)/elapsed_time
   // units: (tick distance)/s
@@ -54,7 +84,7 @@ float Encoder::getSpeed() {
 }
 
 void Encoder::_updateTickDiff() {
-  xTimerReset(_encoderTimer, 10);
+  xTimerReset(_timerHandle, 0);
   portTickType new_tick = xTaskGetTickCountFromISR();
   _tick_diff = new_tick - _last_tick;
   _last_tick = new_tick;
@@ -62,12 +92,21 @@ void Encoder::_updateTickDiff() {
 
 void Encoder::_encoderTimeOut(){
   _tick_diff = 0;
+  _last_tick = xTaskGetTickCountFromISR();
 }
 
-void cbUpdateTickDiff(Encoder* srcEncoder) {
-  srcEncoder->_updateTickDiff();
+void Encoder::registerTimerHandle(TimerHandle_t handle){
+  timerHandleRegister[handle] = this;
 }
 
-void cbEncoderTimeOut(Encoder* srcEncoder){
-  srcEncoder->_encoderTimeOut();
+void Encoder::registerPinIntHandle(uint32_t handle){
+  pinHandleRegister[handle] = this;
+}
+
+void Encoder::unregisterTimerHandle(TimerHandle_t handle){
+  timerHandleRegister[handle] = NULL;
+}
+
+void Encoder::unregisterPinIntHandle(uint32_t handle){
+  pinHandleRegister[handle] = NULL;
 }
--- custom-lib/robot-drivers/encoder.h
+++ custom-lib/robot-drivers/encoder.h
@@ -1,24 +1,39 @@
 #ifndef ENCODER_H
 #define ENCODER_H
 #include <FreeRTOS_SAMD21.h>
+#undef max
+#undef min
+#include <unordered_map>
+
 
 /* Rotary Encoders */
 class Encoder {
+
   public:
     Encoder(const int pin);
     ~Encoder(); 
     float getSpeed();
     void _updateTickDiff();
     void _encoderTimeOut();
-
+    void registerTimerHandle(TimerHandle_t);
+    void registerPinIntHandle(uint32_t);
+    void unregisterTimerHandle(TimerHandle_t);
+    void unregisterPinIntHandle(uint32_t);
+    uint32_t getPinIntHandle();
+    TimerHandle_t getTimerHandle();
+    TickType_t getTickDiff();
   private:
-    TimerHandle_t _encoderTimer;
-    portTickType _last_tick;
-    portTickType _tick_diff;
+    portTickType _last_tick = 0;
+    portTickType _tick_diff = 0;
+    TimerHandle_t _timerHandle;
+    uint32_t _pinIntHandle;
     const int _pin;
   };
 
-static void cbUpdateTickDiff(Encoder* srcEncoder);
-static void cbEncoderTimeOut(Encoder* srcEncoder);
+static std::unordered_map<TimerHandle_t, Encoder*> timerHandleRegister;
+static std::unordered_map<uint32_t, Encoder*> pinHandleRegister;
+
+void cbUpdateTickDiff(uint32_t pinHandle);
+void cbEncoderTimeOut(TimerHandle_t timerHandle);
 
 #endif
